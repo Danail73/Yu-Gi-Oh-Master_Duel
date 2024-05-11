54cc13a82fc7e24026cc3d7cb0baa8ef4cde1d84
Danail73 <dani.marinov.dm@gmail.com>
2024-05-11 12:22:35 +0300

    Card and Monster classes

---
+++ BusinessLayer/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+    public abstract class Card
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string CardText { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+        public Card() {}
+
+        public Card(string name, string cardText)
+            : this()
+        {
+            Name = name;
+            CardText = cardText;
+        }
+    }
+}
---
+++ BusinessLayer/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+    public class Monster:Card
+    {
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Attribute { get; set; }
+
+        [Required]
+        public string Typing { get; set; }
+
+        public int Level { get; set; }
+        public uint ATK { get; set; }
+        public uint DEF { get; set; }
+
+        private Monster() { }
+
+        public Monster(string name, string cardText, string type, string attribute, string typing)
+            :base(name, cardText)
+        {
+            Type = type;
+            Attribute = attribute;
+            Typing = typing;
+        }
+    }
+}
