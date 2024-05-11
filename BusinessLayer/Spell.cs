101ac165285b8c788d152f05212fdf881386da17
Danail73 <dani.marinov.dm@gmail.com>
2024-05-11 12:30:22 +0300

    Spell and Trap classes

---
+++ BusinessLayer/Spell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+    public class Spell:Card
+    {
+        [Required]
+        public string Typing { get; set; }
+
+        private Spell() { }
+
+        public Spell(string name, string cardText, string typing)
+            :base(name, cardText)
+        {
+            Typing = typing;
+        }
+    }
+}
---
+++ BusinessLayer/Trap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+    public class Trap:Card
+    {
+        [Required]
+        public string Typing { get; set; }
+
+        private Trap() { }
+
+        public Trap(string name, string cardText, string typing)
+            :base(name, cardText)
+        {
+            Typing = typing;
+        }
+    }
+}
