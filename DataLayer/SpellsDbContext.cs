1fd6e5b05212fd986581fc3f2e68344e21b60149
Danail73 <dani.marinov.dm@gmail.com>
2024-05-12 16:43:46 +0300

    Db context for spell and trap classes

---
+++ DataLayer/SpellsDbContext.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DataLayer
+{
+    public class SpellsDbContext : IDB<Spell, int>
+    {
+        private readonly CardsDbContext _cardsDbContext;
+        public SpellsDbContext(CardsDbContext cardsDbContext)
+        {
+            _cardsDbContext = cardsDbContext;
+        }
+
+        public void Create(Spell entity)
+        {
+            try
+            {
+                _cardsDbContext.Spells.Add(entity);
+                _cardsDbContext.SaveChanges();
+            }
+            catch (Exception ex) 
+            {
+                throw;
+            }
+        }
+
+        public Spell Read(int key, bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                IQueryable<Spell> query = _cardsDbContext.Spells;
+                return query.SingleOrDefault(s => s.Id == key);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<Spell> ReadAll(bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                IQueryable<Spell> query = _cardsDbContext.Spells;
+                return query.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void Update(Spell entity, bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                Spell spell = Read(entity.Id, useNavigational, false);
+                if(spell is null)
+                {
+                    throw new Exception($"Spell with id = {entity.Id} does not exist");
+                }
+                _cardsDbContext.Spells.Entry(spell).CurrentValues.SetValues(entity);
+                _cardsDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(int key, bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                Spell spell = Read(key, useNavigational, false);
+                if (spell is null)
+                {
+                    throw new Exception($"Spell with id = {key} does not exist");
+                }
+                _cardsDbContext.Spells.Remove(spell);
+                _cardsDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ DataLayer/TrapsDbContext.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DataLayer
+{
+    public class TrapsDbContext : IDB<Trap, int>
+    {
+        private readonly CardsDbContext _cardsDbContext;
+        public TrapsDbContext(CardsDbContext cardsDbContext)
+        {
+            _cardsDbContext = cardsDbContext;
+        }
+
+        public void Create(Trap entity)
+        {
+            try
+            {
+                _cardsDbContext.Traps.Add(entity);
+                _cardsDbContext.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public Trap Read(int key, bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                IQueryable<Trap> query = _cardsDbContext.Traps;
+                return query.SingleOrDefault(t => t.Id == key);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<Trap> ReadAll(bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                IQueryable<Trap> query = _cardsDbContext.Traps;
+                return query.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void Update(Trap entity, bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                Trap trap = Read(entity.Id, useNavigational, false);
+                if (trap is null)
+                {
+                    throw new Exception($"Trap with id = {entity.Id} does not exist");
+                }
+                _cardsDbContext.Traps.Entry(trap).CurrentValues.SetValues(entity);
+                _cardsDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(int key, bool useNavigational = false, bool isReadonly = false)
+        {
+            try
+            {
+                Trap trap = Read(key, useNavigational, false);
+                if (trap is null)
+                {
+                    throw new Exception($"Trap with id = {key} does not exist");
+                }
+                _cardsDbContext.Traps.Remove(trap);
+                _cardsDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
